6fcd256b02d9eb5c1e309df4fb79dfb8c291692f
237-Programming-Students <237tablets@gmail.com>
2026-03-02 18:02:08 -0500

    Added tunable shooter speed control

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -2,11 +2,13 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 
-import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -17,30 +19,44 @@ public class ShooterSubsystem extends SubsystemBase {
 
   SparkMax hopperIntake = new SparkMax(32, MotorType.kBrushless);
 
-  TalonFX backSpinMotor = new TalonFX(21);
-  TalonFX shooterLow = new TalonFX(22);
-  TalonFX shooterHigh = new TalonFX(23);
+  TalonFX shooterBack = new TalonFX(21);
+  TalonFX shooterFeeder = new TalonFX(22);
+  TalonFX shooterFront = new TalonFX(23);
 
-  PIDController shooterSpeedPID = new PIDController(1, 0, 0);
+  private static final double SHOOTER_SPEED_FRONT = 50;
+  private static final double SHOOTER_SPEED_BACK = 100;
+  private static final double FEEDER_DUTY_CYCLE = 1.0;
+  private static final double HOPPER_SPEED = 0.25;
+
+  public ShooterSubsystem() {
+    SmartDashboard.putNumber("Shooter/TargetSpeedFront", SHOOTER_SPEED_FRONT);
+    SmartDashboard.putNumber("Shooter/TargetSpeedBack", SHOOTER_SPEED_BACK);
+    this.shooterFront.getConfigurator().apply(
+        new Slot0Configs().withKV(0.1111111).withKP(0.02));
+    this.shooterBack.getConfigurator().apply(
+        new Slot0Configs().withKV(0.1111111).withKP(0.02));
+  }
 
   public Command spoolShootCommand(double speed) {
     return Commands.run(() -> {
-      shooterHigh.set(1 * speed);
-      backSpinMotor.set(-speed);
+      var targetSpeedFront = SmartDashboard.getNumber("Shooter/TargetSpeedFront", 0.0);
+      var targetSpeedBack = SmartDashboard.getNumber("Shooter/TargetSpeedBack", 0.0);
+      this.shooterFront.setControl(new VelocityVoltage(targetSpeedFront));
+      shooterBack.setControl(new VelocityVoltage(-targetSpeedBack));
     }).finallyDo(() -> {
-      shooterHigh.set(0);
-      backSpinMotor.set(0);
+      shooterFront.set(0);
+      shooterBack.set(0);
     });
   }
 
   public Command shootCommand = Commands.run(() -> {
-    shooterLow.set(-0.5);
+    shooterFeeder.set(-FEEDER_DUTY_CYCLE);
   }).finallyDo(() -> {
-    shooterLow.set(0);
+    shooterFeeder.set(0);
   });
 
   public Command hopperLoadCommand = Commands.run(() -> {
-    hopperIntake.set(-0.25);
+    hopperIntake.set(-HOPPER_SPEED);
   }).finallyDo(() -> {
     hopperIntake.set(0);
   });
